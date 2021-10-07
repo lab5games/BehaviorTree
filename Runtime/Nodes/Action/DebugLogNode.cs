b354e640ecb49386ef75022740dbf81bc70dfc00
Castle <castle.wsc@gmail,com>
2021-10-07 12:47:00 +0800

    update to 1.0.2

--- Runtime/Nodes/Action/DebugLogNode.cs
+++ Runtime/Nodes/Action/DebugLogNode.cs
@@ -3,12 +3,11 @@ namespace Lab5Games.AI
 {
     public class DebugLogNode : ActionNode
     {
-        public ELogType logType = ELogType.Log;
         public string message = "DEBUG LOG NODE";
 
         protected override void OnStart()
         {
-            DebugEx.Log(logType, $"[DebugLogNode]: {message}");
+            UnityEngine.Debug.Log(message);
         }
 
         protected override void OnStop()
